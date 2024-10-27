ca15a49f57824a8503050570d1afec8014153c7b
hy20191108 <57515337+hy20191108@users.noreply.github.com>
2024-10-27 09:47:38 +0900

    Refactor GoogleCalendarApi class

--- lib/google_calendar_api.dart
+++ lib/google_calendar_api.dart
@@ -9,14 +9,13 @@ const _tokenFile = 'token.json';
 
 class GoogleCalendarApi {
   final String credentialsPath;
-  final String eventPath;
   AccessCredentials? credentials;
 
-  GoogleCalendarApi(this.credentialsPath, this.eventPath);
+  GoogleCalendarApi(this.credentialsPath);
 
   Future<void> authenticate() async {
-    final credentialsFile = File(credentialsPath);
-    final credentialsJson = jsonDecode(await credentialsFile.readAsString());
+    final Map<String, dynamic> credentialsJson =
+        jsonDecode(await File(credentialsPath).readAsString());
 
     final clientId = ClientId(
       credentialsJson['installed']['client_id'],
@@ -25,7 +24,7 @@ class GoogleCalendarApi {
 
     // トークンファイルが存在する場合、トークンを読み込む
     if (await File(_tokenFile).exists()) {
-      final tokenJson = jsonDecode(await File(_tokenFile).readAsString());
+      final Map<String, dynamic> tokenJson = jsonDecode(await File(_tokenFile).readAsString());
       credentials = AccessCredentials(
         AccessToken(tokenJson['type'], tokenJson['data'],
             DateTime.parse(tokenJson['expiry'])),
@@ -45,40 +44,32 @@ class GoogleCalendarApi {
       credentials = client.credentials;
 
       // トークンを保存
-      final tokenJson = jsonEncode({
+      final Map<String, dynamic> tokenJson = {
         'type': credentials!.accessToken.type,
         'data': credentials!.accessToken.data,
         'expiry': credentials!.accessToken.expiry.toIso8601String(),
         'refreshToken': credentials!.refreshToken,
-      });
-      await File(_tokenFile).writeAsString(tokenJson);
+      };
+      await File(_tokenFile).writeAsString(jsonEncode(tokenJson));
 
       // クライアントを閉じる
       client.close();
     }
   }
 
-  Future<void> addEvent() async {
+  Future<void> addEvent(String eventFilePath) async {
     var client = authenticatedClient(http.Client(), credentials!);
     var calendarApi = calendar.CalendarApi(client);
 
-    // カレンダーリストを取得
-    var calendarList = await calendarApi.calendarList.list();
-    for (var calendar in calendarList.items!) {
-      print('Calendar: ${calendar.summary} (${calendar.id})');
-    }
-
     // 予定を追加するJSONファイルを読み込む
-    final eventFile = File(eventPath);
-    final eventJson = jsonDecode(await eventFile.readAsString());
+    final String eventJson = await File(eventFilePath).readAsString();
+    final Map<String, dynamic> eventMap = jsonDecode(eventJson);
 
     // 予定を作成
-    var event = calendar.Event.fromJson(eventJson);
+    var event = calendar.Event.fromJson(eventMap);
 
     // 予定をカレンダーに追加
     await calendarApi.events.insert(event, 'primary');
-
-    print('Event added: ${event.summary}');
 
     // クライアントを閉じる
     client.close();
@@ -86,7 +77,7 @@ class GoogleCalendarApi {
 }
 
 void main() async {
-  final api = GoogleCalendarApi('credentials.json', 'sample_event.json');
+  final api = GoogleCalendarApi('credentials.json');
   await api.authenticate();
-  await api.addEvent();
+  await api.addEvent('sample_event.json');
 }
