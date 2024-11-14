12076ee87ddb2587a9936b428349ce00669a2c1c
Haruhiko Yoshioka <zwwp9976@gmail.com>
2024-11-14 23:19:15 +0900

    AndroidManifest.xmlとMainActivity.ktに共有機能を追加

--- android/app/src/main/kotlin/com/example/eventpix_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/eventpix_mobile/MainActivity.kt
@@ -1,5 +1,31 @@
 package com.example.eventpix_mobile
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
+import android.content.Intent
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.shared_text_receiver/text"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getSharedText") {
+                val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+                result.success(sharedText)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
+}
